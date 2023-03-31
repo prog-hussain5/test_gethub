ec3f4fd544cc4d8293c8485cb18c0b2e0502d174
prog hussain <hussaingamer7lion@gmail.com>
2023-03-31 23:34:03 +0300

    ddd

--- test_gethub/Form1.vb
+++ test_gethub/Form1.vb
@@ -1,6 +1,8 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim ali As String
+        Dim n As String
+        Dim mm As String
 
     End Sub
 End Class
